0df1f4b912cd621d91d81f2299cefa567fa6af0b
joaquinpb <joaquinperezbay@gmail.com>
2025-07-29 03:15:33 -0300

    Actualizacion de anon key a nueva bersion de supabase

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Este archivo contiene las claves secretas y NO debe subirse a GitHub.
 
 window.SUPABASE_URL = 'https://ccvfhajwlfhnrmftqpxs.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdmZoYWp3bGZobnJtZnRxcHhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMTQyMDksImV4cCI6MjA2ODc5MDIwOX0._7seIybO8-cvmj0qd4a86Cht63Yl2Y0OPGSenEv9emk';
+window.SUPABASE_ANON_KEY = 'sb_publishable_aVhDjv7UJWjkzoP6xRYuhQ_ZKydUtEh';
