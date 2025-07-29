34b10a8cd6c796c4b619111d40cf1ceb4f0e4b9d
joaquinpb <joaquinperezbay@gmail.com>
2025-07-29 03:28:25 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+// Este archivo contiene las claves secretas y NO debe subirse a GitHub.
+
+window.SUPABASE_URL = 'https://ccvfhajwlfhnrmftqpxs.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_aVhDjv7UJWjkzoP6xRYuhQ_ZKydUtEh';
